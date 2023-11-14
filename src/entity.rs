493b8f3b2126da86de8356a212006f78deff5b2b
Camto <www.ben.pr@gmail.com>
2023-11-14 05:05:41 -0400

    Never qualify Circle.

--- src/entity.rs
+++ src/entity.rs
@@ -3,6 +3,8 @@ pub mod player;
 pub mod guy;
 pub mod pew;
 
+use crate::entity::circle::Circle;
+
 
 pub trait Entity {
 	fn update(&mut self, _: f32) -> Update_Result {
@@ -15,7 +17,7 @@ pub trait Entity {
 	
 	fn get_collision_id(&self) -> i32 { 0 }
 	
-	fn get_hitbox(&self) -> circle::Circle {
+	fn get_hitbox(&self) -> Circle {
 		Default::default()
 	}
 }
