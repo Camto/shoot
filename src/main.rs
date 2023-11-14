e6f89fe757cc656bcd417598f4896aa86b21c049
Camto <www.ben.pr@gmail.com>
2023-11-14 04:31:27 -0400

    Setup for doing collisions. And make window size a global constant.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+
+
 use macroquad::prelude::*;
 
 
 trait Entity {
-	fn update(&mut self, tf: f32) -> Update_Result {
+	fn get_collision_id(&self) -> i32 { 0 }
+	
+	fn get_hitbox(&self) -> Circle {
+		Default::default()
+	}
+	
+	fn update(&mut self, _: f32) -> Update_Result {
 		Default::default()
 	}
 	
@@ -24,27 +34,49 @@ impl Default for Update_Result {
 }
 
 
+#[derive(Copy, Clone)]
 struct Circle {
 	x: f32, y: f32,
 	r: f32
 }
 
 impl Entity for Circle {
+	fn get_hitbox(&self) -> Circle {
+		self.clone()
+	}
+	
 	fn render(&self) {
 		draw_circle(self.x, self.y, self.r, YELLOW);
 	}
 }
 
 impl Circle {
+	fn dist(&self, other: &Self) -> f32 {
+		let x_dist = other.x - self.x;
+		let y_dist = other.y - self.y;
+		(x_dist * x_dist + y_dist * y_dist).sqrt()
+	}
+	
+	fn does_collide(&self, other: &Self) -> bool {
+		self.dist(other) < self.r + other.r
+	}
+	
 	fn off_screen(&self) -> bool {
 		self.x < -500.0 - self.r ||
 			self.y < -500.0 - self.r ||
-			self.x > 1500.0 + self.r ||
-			self.y > 1100.0 + self.r
+			self.x > window_width + 500.0 + self.r ||
+			self.y > window_height + 500.0 + self.r
+	}
+}
+
+impl Default for Circle {
+	fn default() -> Self {
+		Circle { x: 0.0, y: 0.0, r: 15.0 }
 	}
 }
 
 
+const player_coll_id: i32 = 1;
 const player_speed: f32 = 300.0;
 
 struct Player {
@@ -52,6 +84,14 @@ struct Player {
 }
 
 impl Entity for Player {
+	fn get_collision_id(&self) -> i32 {
+		player_coll_id
+	}
+	
+	fn get_hitbox(&self) -> Circle {
+		self.body
+	}
+	
 	fn update(&mut self, tf: f32) -> Update_Result {
 		let speed: f32 =
 			if is_key_down(KeyCode::LeftShift) || is_key_down(KeyCode::RightShift) {
@@ -79,18 +119,18 @@ impl Entity for Player {
 		if self.body.y < -50.0 - self.body.r {
 			self.body.y = -50.0 - self.body.r
 		}
-		if self.body.x > 1100.0 + self.body.r {
-			self.body.x = 1100.0 + self.body.r
+		if self.body.x > window_width + 100.0 + self.body.r {
+			self.body.x = window_width + 100.0 + self.body.r
 		}
-		if self.body.y > 650.0 + self.body.r {
-			self.body.y = 650.0 + self.body.r
+		if self.body.y > window_height + 50.0 + self.body.r {
+			self.body.y = window_height + 50.0 + self.body.r
 		}
 		
-		let cam_x_off = lerp(self.body.x, 0.0, 1000.0, -50.0, 50.0);
-		let cam_y_off = lerp(self.body.y, 0.0, 600.0, -50.0, 50.0);
+		let cam_x_off = lerp(self.body.x, 0.0, window_width, -50.0, 50.0);
+		let cam_y_off = lerp(self.body.y, 0.0, window_height, -50.0, 50.0);
 		set_camera(&Camera3D {
-			position: vec3(500.0 - cam_x_off, 300.0 - cam_y_off, -600.0),
-			target: vec3(500.0, 300.0, 0.0),
+			position: vec3(window_width/2.0 - cam_x_off, window_height/2.0 - cam_y_off, -600.0),
+			target: vec3(window_width/2.0, window_height/2.0, 0.0),
 			up: vec3(0.0, -1.0, 0.0),
 			..Default::default()
 		});
@@ -103,6 +143,8 @@ impl Entity for Player {
 	}
 }
 
+
+const guy_coll_id: i32 = 3;
 
 struct Guy {
 	body: Circle,
@@ -122,6 +164,14 @@ impl Default for Guy {
 }
 
 impl Entity for Guy {
+	fn get_collision_id(&self) -> i32 {
+		guy_coll_id
+	}
+	
+	fn get_hitbox(&self) -> Circle {
+		self.body
+	}
+	
 	fn update(&mut self, tf: f32) -> Update_Result {
 		self.body.x -= 5.0 * tf;
 		
@@ -159,6 +209,8 @@ impl Entity for Guy {
 }
 
 
+const pew_coll_id: i32 = 3;
+
 struct Pew {
 	body: Circle,
 	xv: f32, yv: f32
@@ -174,6 +226,14 @@ impl Default for Pew {
 }
 
 impl Entity for Pew {
+	fn get_collision_id(&self) -> i32 {
+		pew_coll_id
+	}
+	
+	fn get_hitbox(&self) -> Circle {
+		self.body
+	}
+	
 	fn update(&mut self, tf: f32) -> Update_Result {
 		self.body.x += self.xv * tf;
 		self.body.y += self.yv * tf;
@@ -190,9 +250,12 @@ impl Entity for Pew {
 }
 
 
+const window_width: f32 = 1000.0;
+const window_height: f32 = 600.0;
+
 #[macroquad::main("Shoot")]
 async fn main() {
-	request_new_screen_size(1000.0, 600.0);
+	request_new_screen_size(window_width, window_height);
 	next_frame().await;
 	
 	let mut entities: Vec<Box<dyn Entity>> = vec![
@@ -204,8 +267,8 @@ async fn main() {
 		}),
 		Box::new(Guy {
 			body: Circle {
-				x: 1000.0 - 30.0,
-				y: 600.0 - 30.0,
+				x: window_width - 30.0,
+				y: window_height - 30.0,
 				r: 30.0
 			},
 			..Default::default()
@@ -234,14 +297,9 @@ async fn main() {
 		
 		clear_background(RED);
 		
-		draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
-		draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
-		
 		for entity in &entities {
 			entity.render();
 		}
-		
-		draw_text("IT WORKS!", 20.0, 20.0, 30.0, DARKGRAY);
 		
 		next_frame().await
 	}
