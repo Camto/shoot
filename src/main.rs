c4d0966be9e720e77a4cb027c622f1b1c2be7058
Camto <www.ben.pr@gmail.com>
2023-11-14 02:23:58 -0400

    Now it's all a big glob of entities.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
 use macroquad::prelude::*;
 
 
-trait Object {
-	fn update(&mut self, tf: f32);
-	fn render(&self);
-}
-
-trait Enemy: Object {
-	fn shoot(&mut self, tf: f32) -> Vec<Box<dyn Object>>;
+trait Entity {
+	fn update(&mut self, tf: f32) -> Vec<Box<dyn Entity>> {
+		vec![]
+	}
+	
+	fn render(&self) {}
 }
 
 
@@ -16,9 +15,7 @@ struct Circle {
 	r: f32
 }
 
-impl Object for Circle {
-	fn update(&mut self, tf: f32) {}
-	
+impl Entity for Circle {
 	fn render(&self) {
 		draw_circle(self.x, self.y, self.r, YELLOW);
 	}
@@ -31,8 +28,8 @@ struct Player {
 	body: Circle
 }
 
-impl Object for Player {
-	fn update(&mut self, tf: f32) {
+impl Entity for Player {
+	fn update(&mut self, tf: f32) -> Vec<Box<dyn Entity>> {
 		let speed: f32 =
 			if is_key_down(KeyCode::LeftShift) || is_key_down(KeyCode::RightShift) {
 				player_speed * 0.5
@@ -52,6 +49,17 @@ impl Object for Player {
 		if is_key_down(KeyCode::Right) {
 			self.body.x += speed * tf;
 		}
+		
+		let cam_x_off = lerp(self.body.x, 0.0, 1000.0, -50.0, 50.0);
+		let cam_y_off = lerp(self.body.y, 0.0, 600.0, -50.0, 50.0);
+		set_camera(&Camera3D {
+			position: vec3(500.0 + cam_x_off, 300.0 + cam_y_off, -600.0),
+			target: vec3(500.0, 300.0, 0.0),
+			up: vec3(0.0, -1.0, 0.0),
+			..Default::default()
+		});
+		
+		vec![]
 	}
 	
 	fn render(&self) {
@@ -77,20 +85,11 @@ impl Default for Guy {
 	}
 }
 
-impl Object for Guy {
-	fn update(&mut self, tf: f32) {
+impl Entity for Guy {
+	fn update(&mut self, tf: f32) -> Vec<Box<dyn Entity>> {
 		self.body.x -= 5.0 * tf;
-	}
-	
-	fn render(&self) {
-		self.body.render();
-	}
-}
-
-impl Enemy for Guy {
-	fn shoot(&mut self, tf: f32) -> Vec<Box<dyn Object>> {
-		self.shoot_timer += tf;
 		
+		self.shoot_timer += tf;
 		if self.shoot_timer >= self.shoot_cycle {
 			self.shoot_timer = 0.0;
 			
@@ -114,6 +113,10 @@ impl Enemy for Guy {
 			vec![]
 		}
 	}
+	
+	fn render(&self) {
+		self.body.render();
+	}
 }
 
 
@@ -131,10 +134,12 @@ impl Default for Pew {
 	}
 }
 
-impl Object for Pew {
-	fn update(&mut self, tf: f32) {
+impl Entity for Pew {
+	fn update(&mut self, tf: f32) -> Vec<Box<dyn Entity>> {
 		self.body.x += self.xv * tf;
 		self.body.y += self.yv * tf;
+		
+		vec![]
 	}
 	
 	fn render(&self) {
@@ -148,14 +153,13 @@ async fn main() {
 	request_new_screen_size(1000.0, 600.0);
 	next_frame().await;
 	
-	let mut player: Player = Player {
-		body: Circle {
-			x: 200.0, y: 300.0,
-			r: 15.0
-		}
-	};
-	
-	let mut enemies: Vec<Box<dyn Enemy>> = vec![
+	let mut entities: Vec<Box<dyn Entity>> = vec![
+		Box::new(Player {
+			body: Circle {
+				x: 200.0, y: 300.0,
+				r: 15.0
+			}
+		}),
 		Box::new(Guy {
 			body: Circle {
 				x: 1000.0 - 30.0,
@@ -166,38 +170,26 @@ async fn main() {
 		})
 	];
 	
-	let mut bullets: Vec<Box<dyn Object>> = vec![];
+	let mut bullets: Vec<Box<dyn Entity>> = vec![];
 	
 	loop {
-		clear_background(RED);
+		let tf: f32 = get_frame_time();
 		
-		let cam_x_off = lerp(player.body.x, 0.0, 1000.0, -50.0, 50.0);
-		let cam_y_off = lerp(player.body.y, 0.0, 600.0, -50.0, 50.0);
-		set_camera(&Camera3D {
-			position: vec3(500.0 + cam_x_off, 300.0 + cam_y_off, -600.0),
-			target: vec3(500.0, 300.0, 0.0),
-			up: vec3(0.0, -1.0, 0.0),
-			..Default::default()
-		});
+		let mut new_entities = vec![];
+		for entity in &mut entities {
+			new_entities.append(&mut entity.update(tf));
+		}
+		
+		clear_background(RED);
 		
 		draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
 		draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
 		
-		let tf: f32 = get_frame_time();
-		
-		player.update(tf);
-		player.render();
-		
-		for enemy in enemies.iter_mut() {
-			enemy.update(tf);
-			bullets.append(&mut enemy.shoot(tf));
-			enemy.render();
+		for entity in &entities {
+			entity.render();
 		}
 		
-		for bullet in bullets.iter_mut() {
-			bullet.update(tf);
-			bullet.render();
-		}
+		entities.append(&mut new_entities);
 		
 		draw_text("IT WORKS!", 20.0, 20.0, 30.0, DARKGRAY);
 		
