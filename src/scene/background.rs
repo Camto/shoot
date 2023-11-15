67cac0ea7bd490b58bc8db7a7f098a296682177b
Camto <www.ben.pr@gmail.com>
2023-11-15 02:13:18 -0400

    Red enemy was due to 3d Z issues. Should go back and have some system for ensuring things in the back are drawn first but not going to go back and make a rigorous fix for that.

--- src/scene/background.rs
+++ src/scene/background.rs
@@ -1,13 +1,16 @@
 use crate::entity;
+use crate::entity::Entity;
 use crate::entity::background::Background;
 use crate::scene;
 
 
 pub fn add(entities: &mut scene::Entities, scroll_speed: f32, texs: &entity::Textures) {
-	entities[0].append(&mut vec![
+	let mut prepended_entities: Vec<Box<dyn Entity>> = vec![
 		Box::new(Background { tex_id: 0, tex_width: texs[0].width(), offset: -texs[0].width(), scroll_speed }),
 		Box::new(Background { tex_id: 0, tex_width: texs[0].width(), offset: 0.0, scroll_speed }),
 		Box::new(Background { tex_id: 0, tex_width: texs[0].width(), offset: texs[0].width(), scroll_speed }),
 		Box::new(Background { tex_id: 0, tex_width: texs[0].width(), offset: 2.0 * texs[0].width(), scroll_speed })
-	])
+	];
+	prepended_entities.append(&mut entities[0]);
+	entities[0] = prepended_entities
 }
